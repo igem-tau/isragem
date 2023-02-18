fc9c53e1e51d9db50062e8e428f7bf879ac743c6
iGem <igem@davids-MacBook-Pro-2011.local>
2023-02-18 21:42:32 +0200

    separate the first card in the overview section

--- components/Overview/index.jsx
+++ components/Overview/index.jsx
@@ -5,13 +5,13 @@ export default function Overview() {
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>על קצה המזלג</h2>
+      <PictureCard
+        text='פרויקט ה- iGEM הינו פרויקט ייחודי, בו סטודנטים מאוניברסיטאות מכל העולם מתבקשים ליזום פרויקט מחקר בתחום הביולוגיה הסינטטית, ולבסוף להציג את תוצאותיהם בתחרות בינלאומית המתקיימת מדי שנה בפריז. הרעיון מאחורי תחרות הISRAGEM הוא לאפשר לתלמידי תיכון בישראל להתנסות בחוויה דומה.'
+        imagePath='/iGem_light_Logo.svg'
+        imageDescription='iGem logo'
+        className={`${styles.large_card} ${styles.single_card}`}
+      />
       <div className={styles.cards}>
-        <PictureCard
-          text='פרויקט ה- iGEM הינו פרויקט ייחודי, בו סטודנטים מאוניברסיטאות מכל העולם מתבקשים ליזום פרויקט מחקר בתחום הביולוגיה הסינטטית, ולבסוף להציג את תוצאותיהם בתחרות בינלאומית המתקיימת מדי שנה בפריז. הרעיון מאחורי תחרות הISRAGEM הוא לאפשר לתלמידי תיכון בישראל להתנסות בחוויה דומה.'
-          imagePath='/iGem_light_Logo.svg'
-          imageDescription='iGem logo'
-          className={styles.large_card}
-        />
         <PictureCard
           text='תחום הביולוגיה הסינטטית הוא תחום מולטי-דיסיפילנרי וצעיר המשלב את עולם ההנדסה ומדעי המחשב עם מדעי החיים. הביולוגיה הסינטטית משלבת רעיונות ועקרונות מהעולמות השונים במטרה לתכנן ולבנות רכיבים או מערכות שלמות שלא ניתן למצוא כדוגמתם בטבע.'
           imagePath='/left_tip_image.svg'
