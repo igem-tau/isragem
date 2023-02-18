e645162bf46ac05e73aa606fec50887c848d1751
iGem <igem@davids-MacBook-Pro-2011.local>
2023-02-18 16:55:16 +0200

    upadte the initial state of the countdown to "--"

--- components/CountDown/index.jsx
+++ components/CountDown/index.jsx
@@ -49,10 +49,10 @@ export default function CountDown({
   minutesText = "minutes",
   secondsText = "seconds",
 }) {
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const [days, setDays] = useState("--");
+  const [hours, setHours] = useState("--");
+  const [minutes, setMinutes] = useState("--");
+  const [seconds, setSeconds] = useState("--");
 
   useEffect(() => {
     const timeInterval = setInterval(() => {
