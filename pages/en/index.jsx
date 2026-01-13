2fddfa4914b47d39d86a3a3eddb30a5b1ebb72e7
altarh <altarh@mail.tau.ac.il>
2026-01-13 09:51:18 +0200

    removed supporting documents

--- pages/en/index.jsx
+++ pages/en/index.jsx
@@ -72,7 +72,7 @@ const tutorialData = {
       src: "https://drive.google.com/file/d/1LVEdWSlsXRD9S20gqd7tsAKwu7Eww8bH/preview",
     },
   ],
-  videoInstructionsText: "Supporting documents - explanations and background topics",
+  videoInstructionsText: "explanations and background topics",
   videoInstructionsLink:
     "https://pop.education.gov.il/tchumey_daat/biologya/chativa-elyona/noseem-nilmadim/",
 };
