dbcab5fceefef2a1f68a4e55bad15bfa1a10dc45
altarh <altarh@mail.tau.ac.il>
2026-01-20 11:00:29 +0200

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Karantina } from "@next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 
 const karantina = Karantina({
   display: "swap",
