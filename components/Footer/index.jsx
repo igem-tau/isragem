fbf05e4a643530b66d52732667fd8814b59b66e6
altarh <altarh@mail.tau.ac.il>
2026-01-20 11:15:53 +0200

    added escape room sponser sentence

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -21,6 +21,7 @@ export default function Footer({ credits }) {
             )
           )}
       </div>
+
       <a href='https://www.tau.ac.il/' target='_blank'>
         <Image
           src='/TAU_Logo.svg'
@@ -29,7 +30,8 @@ export default function Footer({ credits }) {
           height={35}
         />
       </a>
-        <a href='https://www.ort.org.il/' target='_blank'>
+
+      <a href='https://www.ort.org.il/' target='_blank'>
         <Image
           src='/ORT_Logo.png'
           alt='Ort logo'
@@ -37,6 +39,7 @@ export default function Footer({ credits }) {
           height={35}
         />
       </a>
+
       <a href='https://igem.org/' target='_blank'>
         <Image
           src='/iGem_dark_Logo.svg'
@@ -45,23 +48,31 @@ export default function Footer({ credits }) {
           height={35}
         />
       </a>
-            <a href='https://www.escaperoom.co.il/escape-rooms' target='_blank'>
-        <Image
-          src='/ESCAPE_Logo.png'
-          alt='Escape Room logo'
-          width={185}
-          height={35}
-        />
-      </a>
-        {/* <a href='https://www.alagene.co.il/' target='_blank'>
-        <Image
-          src='/Alagene_Logo.svg'
-          alt='Alagene logo'
-          width={100}
-          height={35}
-        />
-      </a>
-      */}
+
+      {/* --- Start of Escape Room Section --- */}
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <a href='https://www.escaperoom.co.il/escape-rooms' target='_blank'>
+          <Image
+            src='/ESCAPE_Logo.png'
+            alt='Escape Room logo'
+            width={185}
+            height={35}
+          />
+        </a>
+        <p style={{
+            fontSize: '10px',
+            textAlign: 'center',
+            marginTop: '5px',
+            marginBottom: '0',
+            maxWidth: '185px', // Keeps text same width as logo
+            lineHeight: '1.2',
+            direction: 'rtl'
+        }}>
+          בחסות אסקייפ רום בע"ם <br /> רשת חדרי הבריחה הגדולה בארץ
+        </p>
+      </div>
+      {/* --- End of Escape Room Section --- */}
+
     </footer>
   );
 }
